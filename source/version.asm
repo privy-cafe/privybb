3ad17b7e061ad2596e65b7fe3843b1851ffe73a7
PrivyERA <privyedge@tuta.io>
2020-02-05 04:23:37 -0500

    Update version.asm
    version changes

--- source/version.asm
+++ source/version.asm
@@ -2,7 +2,7 @@
 iglobal
 
   if used cVersion
-    cVersion  db   '<b>PrivyBB v0.1</b> (check-in: <a href="github.com/privy.cafe/ASMBB-Privy'
+    cVersion  db   '<b>PrivyBB v0.2</b> (check-in: <a href="github.com/privy.cafe/ASMBB-Privy'
               file "../manifest.uuid":0,16
               db   '">'
               file "../manifest.uuid":0,16
